18e998c5b154de209e28aaa6b1f9d8960dd4f779
unknown <bkinrewin@gmail.com>
2022-03-12 18:27:55 -0500

    handlebars

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,99 +1,98 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { Post, User, Comment, Vote } = require('../models');
-const withAuth = require('../utils/auth');
-
-// get all posts for dashboard
-router.get('/', withAuth, (req, res) => {
-  console.log(req.session);
-  console.log('======================');
-  Post.findAll({
-    where: {
-      user_id: req.session.user_id,
-    },
-    attributes: [
-      'id',
-      'post_url',
-      'title',
-      'created_at',
-      [
-        sequelize.literal(
-          '(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'
-        ),
-        'vote_count',
-      ],
-    ],
-    include: [
-      {
-        model: Comment,
-        attributes: [
-          'id',
-          'comment_text',
-          'post_id',
-          'user_id',
-          'created_at',
-        ],
-        include: {
-          model: User,
-          attributes: ['username'],
-        },
-      },
-      {
-        model: User,
-        attributes: ['username'],
-      },
-    ],
-  })
-    .then(dbPostData => {
-      const posts = dbPostData.map(post => post.get({ plain: true }));
-      res.render('dashboard', { posts, loggedIn: true });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, User, Comment, Vote } = require("../models");
+const withAuth = require("../utils/auth");
+router.get("/", (req, res) => {
+  const data = require("../message.json");
+  const gamereleases = data.results;
+  res.render("dashboard", { gamerelease });
 });
+//
+// get all posts for dashboard
+// router.get('/', withAuth, (req, res) => {
+//   console.log(req.session);
+//   console.log('======================');
+//   Post.findAll({
+//     where: {
+//       user_id: req.session.user_id,
+//     },
+//     attributes: [
+//       'id',
+//       'post_url',
+//       'title',
+//       'created_at',
+//       [
+//         sequelize.literal(
+//           '(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'
+//         ),
+//         'vote_count',
+//       ],
+//     ],
+//     include: [
+//       {
+//         model: Comment,
+//         attributes: [
+//           'id',
+//           'comment_text',
+//           'post_id',
+//           'user_id',
+//           'created_at',
+//         ],
+//         include: {
+//           model: User,
+//           attributes: ['username'],
+//         },
+//       },
+//       {
+//         model: User,
+//         attributes: ['username'],
+//       },
+//     ],
+//   })
+//     .then(dbPostData => {
+//       const posts = dbPostData.map(post => post.get({ plain: true }));
+//       res.render('dashboard', { posts, loggedIn: true });
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
 
-router.get('/edit/:id', withAuth, (req, res) => {
+router.get("/edit/:id", withAuth, (req, res) => {
   Post.findByPk(req.params.id, {
     attributes: [
-      'id',
-      'post_url',
-      'title',
-      'created_at',
+      "id",
+      "post_url",
+      "title",
+      "created_at",
       [
         sequelize.literal(
-          '(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'
+          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
         ),
-        'vote_count',
+        "vote_count",
       ],
     ],
     include: [
       {
         model: Comment,
-        attributes: [
-          'id',
-          'comment_text',
-          'post_id',
-          'user_id',
-          'created_at',
-        ],
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
         include: {
           model: User,
-          attributes: ['username'],
+          attributes: ["username"],
         },
       },
       {
         model: User,
-        attributes: ['username'],
+        attributes: ["username"],
       },
     ],
   })
-    .then(dbPostData => {
+    .then((dbPostData) => {
       if (dbPostData) {
         const post = dbPostData.get({ plain: true });
 
-        res.render('edit-post', {
+        res.render("edit-post", {
           post,
           loggedIn: true,
         });
@@ -101,7 +100,7 @@ router.get('/edit/:id', withAuth, (req, res) => {
         res.status(404).end();
       }
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).json(err);
     });
 });

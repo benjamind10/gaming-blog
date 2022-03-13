8e66c354f5d9981b6b45c7e14848ea2bca60a316
Ben <benjamind10@pm.me>
2022-03-13 11:17:51 -0400

    fixed release date filter for 2022

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -48,7 +48,7 @@ router.get('/', (req, res) => {
     try {
       const fetch = axios
         .get(
-          `http://www.gamespot.com/api/games/?api_key=${process.env.GAMESPOT_API}&format=json&limit=3`
+          `http://www.gamespot.com/api/games/?api_key=${process.env.GAMESPOT_API}&format=json&limit=10&filter=release_date:2022-01-01|2022-02-02`
         )
         .then(response => {
           releases.push(response.data);

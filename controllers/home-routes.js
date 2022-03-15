e0bcfde3744e4a7d23b233e7e27c9e8fd49cf5c3
Ben <benjamind10@pm.me>
2022-03-15 11:16:24 -0400

    able to show forum post on page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -157,7 +157,52 @@ router.get('/forum', withAuth, (req, res) => {
     username: req.session.username,
   };
 
-  res.render('forum', { userObj });
+  Post.findAll({
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [
+        sequelize.literal(
+          '(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'
+        ),
+        'vote_count',
+      ],
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: [
+          'id',
+          'comment_text',
+          'post_id',
+          'user_id',
+          'created_at',
+        ],
+        include: {
+          model: User,
+          attributes: ['username'],
+        },
+      },
+      {
+        model: User,
+        attributes: ['username'],
+      },
+    ],
+  })
+    .then(dbPostData => {
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+
+      res.render('forum', {
+        posts,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;

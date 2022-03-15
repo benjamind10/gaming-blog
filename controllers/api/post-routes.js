445fdf90c5607997de4a64bd1b0c00d5515ac610
Nathaniel Boucher <93409883+natejboucher@users.noreply.github.com>
2022-03-15 12:34:17 -0400

    fixed post routes and model

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -80,12 +80,12 @@ router.post('/', withAuth, (req, res) => {
   // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
   Post.create({
     title: req.body.title,
-    post_url: req.body.post_url,
+    post_url: req.body.post_content,
     user_id: req.session.user_id
   })
     .then(dbPostData => res.json(dbPostData))
     .catch(err => {
-      console.log(err);
+      console.log("Error on API",err);
       res.status(500).json(err);
     });
 });
--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,18 +1,14 @@
 async function newFormHandler(event) {
   event.preventDefault();
 
-  const title = document.querySelector(
-    'input[name="post-title"]'
-  ).value;
-  const post_url = document.querySelector(
-    'input[name="post-url"]'
-  ).value;
-
+  const title = document.querySelector('#casual-post-title').value;
+  const post_content = document.querySelector('#casual-post-content').value;
+  console.log(title, post_content);
   const response = await fetch(`/api/posts`, {
     method: 'POST',
     body: JSON.stringify({
       title,
-      post_url,
+      post_content,
     }),
     headers: {
       'Content-Type': 'application/json',
